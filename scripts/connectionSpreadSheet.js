5bc2eebef8ce51998c6bb49a5564022790f41347
Sebastian Talamona <sebastiantalamona@gmail.com>
2022-11-21 15:00:24 -0300

    Cambio de spreadsheet

--- scripts/connectionSpreadSheet.js
+++ scripts/connectionSpreadSheet.js
@@ -8,7 +8,7 @@ const req = async (param) => {
     try{
         x.classList.add('off');
         spiner.classList.remove('off');
-        const url = 'https://script.google.com/macros/s/AKfycbwJYfG7qvOliQFSZp7nkI4WKN5O0McTsUoDFjQjOrD9sX3TieaFmKpvFAcxxNJcQdZv/exec';
+        const url = 'https://script.google.com/macros/s/AKfycbxuS8ViXPQuZZ97BNlqZkQEQsj_emJzKXV4cpRZwwLOEgZCJct2GrYkCal-lgvsaWm_/exec';
         const res = await fetch(url + param);
         const resJson = await res.json(); 
         return resJson;
